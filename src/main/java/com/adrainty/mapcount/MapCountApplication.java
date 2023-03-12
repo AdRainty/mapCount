644a45e143b64edce2d6d9002f785421b7794b9c
adrainty <lemon_173@126.com>
2023-03-13 00:12:46 +0800

    first commit

---
+++ src/main/java/com/adrainty/mapcount/MapCountApplication.java
@@ -0,0 +1,13 @@
+package com.adrainty.mapcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapCountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapCountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adrainty/mapcount/MapCountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrainty.mapcount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapCountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
